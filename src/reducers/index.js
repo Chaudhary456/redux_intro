56e9a26d60c54bc6bb0244c1a5815a9b83983222
Chaudhary456 <schaudhary332001@gmail.com>
2022-12-04 12:37:42 +0530

    Init commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./updown";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default rootReducer;
